e6652022f3aa69665bda1ffdd4a85eff9980e847
Andreas Kristiansen <mail@ankr.dk>
2024-07-24 21:29:10 +0200

    Able to talk to db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+const prisma = new PrismaClient();
+
+app.get('/', async (req: Request, res: Response) => {
+  await prisma.guest.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@wonderland.com',
+    },
+  });
+
+  const guests = await prisma.guest.findMany();
+
+  res.send(guests);
 });
 
 app.listen(port, () => {
