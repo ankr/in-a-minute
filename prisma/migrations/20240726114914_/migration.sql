5f38a6499301d3e5a4e8417d6a4e73d9b4101668
Andreas Kristiansen <mail@ankr.dk>
2024-07-26 13:49:26 +0200

    Reset db

--- prisma/migrations/20240726114914_/migration.sql
+++ prisma/migrations/20240726114914_/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "Properties" (
     "id" SERIAL NOT NULL,
+    "owner_id" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -43,6 +44,9 @@ CREATE TABLE "Messages" (
 
     CONSTRAINT "Messages_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "Properties" ADD CONSTRAINT "Properties_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "Guests"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Reservations" ADD CONSTRAINT "Reservations_guest_id_fkey" FOREIGN KEY ("guest_id") REFERENCES "Guests"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
